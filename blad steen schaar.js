ab16a9768aad9676066ce1704acc12f6b2230533
Pieter Bouckaert <bouckaert.pieter@vdabcampus.be>
2023-09-14 13:57:34 +0200

    ok

--- blad steen schaar.js
+++ blad steen schaar.js
@@ -1,29 +1,24 @@
 "use strict";
-let keuze = (0);
+let keuze = 0;
 
-document.getElementById("blad").onclick = function () { verander1(); keuzePc(); }
-
-document.getElementById("steen").onclick = function () { verander2(); keuzePc(); }
-document.getElementById("schaar").onclick = function () { verander3(); keuzePc(); }
-
-function verander1() {
-    keuze = 1;
+const buttons = document.querySelectorAll("button");
+for (const button of buttons) {
+    button.onclick = verwerkkeuze;
 }
-function verander2() {
-    keuze = 2;
-}
-function verander3() {
-    keuze = 3;
+function verwerkkeuze (){
+    keuze = Number(this.dataset.nr);
+    keuzePc();
+
 }
 function keuzePc() {
+    const afbeeldingen = [ "blad","steen","schaar"]
+    const uitslag= document.getElementById("uitslag")
     const kpc = Math.floor((Math.random() * 3) + 1)
     const img = document.getElementById("afbeelding");
-    if (kpc === 1) { img.src = `blad.png`; }
-    if (kpc === 2) { img.src = `steen.png`; }
-    if (kpc === 3) { img.src = `schaar.png`; }
+    img.src = `${afbeeldingen[kpc-1]}.png`;
 
-if(keuze === 1 && kpc === 2 || keuze === 2 && kpc === 3 || keuze === 3 && kpc === 1 ){document.getElementById("uitslag").innerText = ("gewonnen");}
-if(keuze === 2 && kpc === 1 || keuze === 3 && kpc === 2 || keuze === 1 && kpc === 3 ){ document.getElementById("uitslag").innerText = ("verloren");}
-if(keuze === 1 && kpc === 1 || keuze === 2 && kpc === 2 || keuze === 3 && kpc === 3 ){  document.getElementById("uitslag").innerText = ("gelijk");}
+    if (keuze === 1 && kpc === 2 || keuze === 2 && kpc === 3 || keuze === 3 && kpc === 1) { uitslag.innerText = "gewonnen"; }
+    if (keuze === 2 && kpc === 1 || keuze === 3 && kpc === 2 || keuze === 1 && kpc === 3) { uitslag.innerText = "verloren"; }
+    if (keuze === kpc) { uitslag.innerText = "gelijk"; }
 }
 
